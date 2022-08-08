4d60303003ccef099b79fffccb0bbcfd8b45353d
Chamath Palihawadana <chamathpali123@gmail.com>
2022-08-08 11:55:12 +0100

    Folder based query support added

--- clood-query/app.py
+++ clood-query/app.py
@@ -1,14 +1,17 @@
 from clood import Clood
 import json
+import os
+import sys 
 
 ##### CONFIGURATION #####
-CLOOD_PROJECT_ID = "cf9c88425c214a57a22c76dc00de2caa"
-CLOOD_API_URL    = "http://localhost:3000/dev"
+CLOOD_PROJECT_ID = "01e420ad086349be83671e4926670813"
+FOLDER_NAME = "RandomQuery"
 FILE_NAME = "randcase.json"
-topK = 5
+topK = 3
 #########################
 
 
+CLOOD_API_URL    = "http://localhost:3000/dev"
 
 print("-------------------------------------------")
 
@@ -18,11 +21,41 @@ clood = Clood(CLOOD_PROJECT_ID, CLOOD_API_URL)
 # clood.seedcases()
 
 # Sample Case Structure conversion
-# query_case = clood.convert_to_clood(FILE_NAME)
-# print(json.dumps(query_case, indent=4))
+query_case = clood.convert_to_clood(FILE_NAME)
+print(json.dumps(query_case, indent=4))
 
 # Query Case Base
-retrieved_cases = clood.query(file=FILE_NAME, topK= topK , soln= ["SolutionExplanation", "SolutionBT"])
+# retrieved_cases = clood.query(file=FILE_NAME, topK= topK , soln= ["SolutionExplanation", "SolutionBT"])
+# for c in retrieved_cases:
+#     print(c["Name"], c["score__"])
+
 # print(json.dumps(retrieved_cases, indent=4))
 
-clood.export_cases(FILE_NAME, retrieved_cases)
+# clood.export_cases(FILE_NAME, retrieved_cases)
+
+
+# stdoutOrigin=sys.stdout 
+# sys.stdout = open("log.txt", "w")
+count = 0
+cases = 0
+## Browser through iSee Generated Random Queries
+for subdir, dirs, files in os.walk(FOLDER_NAME):
+    for f in files:
+         if f.lower().endswith((".json")):
+            print("----------------------")                    
+            print("File =>", subdir+" -> "+f)
+            count += 1
+            # Query Case Base
+            retrieved_cases = clood.query(file=os.path.join(subdir, f), topK= topK , soln= ["SolutionExplanation", "SolutionBT"])
+            for c in retrieved_cases:
+                print(c["Name"], c["score__"])
+                cases += 1
+                
+            print("----------------------\n")                    
+
+print("\n\n--------Summary--------\n")                    
+print("Total Queries Ran = ", str(count))
+print("Total Retrieved Cases = ", str(cases))
+print("----------------------\n")                    
+# sys.stdout.close()
+# sys.stdout=stdoutOrigin
